19277ff196258d8102ddfbeb3797e2749b987fa7
Vaibhav <vaibhav.pandey0806@gmail.com>
2024-10-22 14:43:42 +0530

    update: swagger files for auth route

--- app/api-docs/page.tsx
+++ app/api-docs/page.tsx
@@ -43,7 +43,7 @@ const App = () => {
       <ApiReferenceReact
         configuration={{
           spec: {
-            url: 'https://docs.okto.tech/api-sec',
+            url: 'http://localhost:3000/api-sec',
           },
           theme: 'none',
           darkMode: theme === 'dark',
