990a4754d64aad26c6ca32ddae9bf4132c61670a
Ae0h <dontlisten33@gmail.com>
2024-11-21 16:04:46 -0800

    Wip

--- app/components/AskCookbook.tsx
+++ app/components/AskCookbook.tsx
@@ -1,17 +1,11 @@
 "use client"
 
 import React from "react";
-import dynamic from "next/dynamic";
+import BaseAskCookbook  from "@cookbookdev/docsbot/react";
 
-const BaseAskCookbook = dynamic(
-  () => import("@cookbookdev/docsbot/react"),
-  {
-    ssr: false,
-  },
-);
+const COOKBOOK_PUBLIC_API_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NmY0NTc5OGJkODc1Y2E3NmUwMGFmOWYiLCJpYXQiOjE3MjcyODkyNDAsImV4cCI6MjA0Mjg2NTI0MH0.jjwNwuxA0WhC8yuHDgXLmFfKj83_qUoiYj7FoCKx88k";
 
 export default function AskCookbook() {
-  const COOKBOOK_PUBLIC_API_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NmY0NTc5OGJkODc1Y2E3NmUwMGFmOWYiLCJpYXQiOjE3MjcyODkyNDAsImV4cCI6MjA0Mjg2NTI0MH0.jjwNwuxA0WhC8yuHDgXLmFfKj83_qUoiYj7FoCKx88k";
   return <BaseAskCookbook apiKey={COOKBOOK_PUBLIC_API_KEY} />;
 }
